9b6c1fa936104938aa3e4b23263253628e57ef44
Kyle Gorak <kyle@gorak.us>
2015-09-28 14:31:07 -0400

    added admin CP

--- codenames/admin.py
+++ codenames/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Agency, Location, Operation
+
+class AgencyAdmin(admin.ModelAdmin):
+	fields=['name']
+
+class LocationAdmin(admin.ModelAdmin):
+	fields=['country', 'longitude', 'latitude']
+
+class OperationAdmin(admin.ModelAdmin):
+	fieldsets=[
+		(None, {'fields':['code_word']}),
+		('Operation Information', {'fields':['op_num','clearance','agency','user','location'],
+									'classes':['collapse']}),
+	]
+
+admin.site.register(Agency, AgencyAdmin)
+admin.site.register(Location, LocationAdmin)
+admin.site.register(Operation, OperationAdmin)
