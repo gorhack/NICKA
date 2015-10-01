780deec43c3d65b3c6ce1d6dd954a9706c429cda
Kyle Gorak <kyle@gorak.us>
2015-10-01 19:54:02 -0400

    Changed index page to under construction

--- codenames/views.py
+++ codenames/views.py
@@ -3,7 +3,20 @@ from django.http import HttpResponse
 # Create your views here.
 def index_page(request):
 	# TODO:// implement templates
-	return HttpResponse("Index...")
+	output = '''
+	    <html>
+	      <head><title>%s</title></head>
+	      <body>
+	        <h1>%s</h1><p>%s</p>
+	      </body>
+	    </html>
+	  ''' % (
+	    'NICKA Index',
+	    'NICKA is Under Construction',
+	    'This website is currently under construction. Please try again later.'
+  )
+
+	return HttpResponse(output)
 
 def op_detail_page(request, operation_id):
 	# TODO:// implement view page by op code
